f845febf99fb285ba0dd453699ac63062fecef8d
Jonathan Gama <jonathan.gama@liqi.com.br>
2023-02-13 00:09:11 -0400

    remove other svg/jsx icons

--- src/icons/big/index..jsx
+++
@@ -1,7 +0,0 @@
-export { default as ActiveToken } from "./ActiveToken.";
-export { default as Calendar } from "./Calendar.";
-export { default as CircleMoney } from "./CircleMoney.";
-export { default as Receivables } from "./Receivables.";
-export { default as TokenFamily } from "./TokenFamily.";
-export { default as Token } from "./Token.";
-export { default as YourMoney } from "./YourMoney.";
